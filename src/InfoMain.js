ffd3f7b5b056373e15bfc2bf4ac0e0df05b1c2e4
nadiacoralves <55755193+nadiacoralves@users.noreply.github.com>
2020-09-13 18:45:30 +0100

    css components adjustment

--- src/InfoMain.js
+++ src/InfoMain.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react';
-import Curriculum from './Curriculum';
-import Search from './Search';
-import './InfoMain.css';
+import React, { Component } from "react";
+import Curriculum from "./Curriculum";
+import Search from "./Search";
+import "./InfoMain.css";
 
 class InfoPage extends Component {
-	render() {
-		return (
-			<div>
-				<div className="header">
-					<header>
-						<a href="https://www.correiagoncalves.com" style={{ textDecoration: 'none', color: 'black' }}>
-							Na'dia
-							<br />
-							Correia Gonc,alves
-						</a>
-					</header>
-				</div>
-				<Search />
-				<div className="row">
-					<div className="side">
-						<h2>
-							<Curriculum />
-						</h2>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div class="mainHeader">
+        <div className="header">
+          <header>
+            <a
+              href="https://www.correiagoncalves.com"
+              style={{ textDecoration: "none", color: "black" }}
+            >
+              Na'dia
+              <br />
+              Correia Gonc,alves
+            </a>
+          </header>
+        </div>
+        <Search />
+        <div className="row">
+          <div className="side">
+            <h2>
+              <Curriculum />
+            </h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default InfoPage;
--- src/Search.js
+++ src/Search.js
@@ -1,50 +1,55 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import './Search.css';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import "./Search.css";
 
 class Search extends Component {
-	state = '';
-	preventReload = event => {
-		event.preventDefault();
-		this.setState({});
-	};
+  state = "";
+  preventReload = (event) => {
+    event.preventDefault();
+    this.setState({});
+  };
 
-	render() {
-		return (
-			<div className="searchBar">
-				<h1 style={{ margin: 0 }}>
-					<div className="clearfix">
-						<form onSubmit={this.preventReload}>
-							<input className="emailInput float-left" type="text" placeholder="Email" />
-							<a
-								href="mailto:nadiacoralves@gmail.com?Subject=Hi%20Nadia"
-								target="_top"
-								className="submitEmail"
-								style={{ color: 'black' }}
-							>
-								<FontAwesomeIcon icon={faChevronRight} size="1x" />
-							</a>
-						</form>
-						<span className="infoIcon float-left ml-3">
-							<FontAwesomeIcon icon={faInfoCircle} size="1x" />
-						</span>
-						<span className="avocadoIcon float-left ml-3" role="img" aria-label="Avocado">
-							<a
-								href="https://nadiacoralves.tumblr.com/"
-								target="_blank"
-								rel="noopener noreferrer"
-								style={{ textDecoration: 'none' }}
-							>
-								🥑
-							</a>
-						</span>
-					</div>
-				</h1>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="searchBar">
+        <h1 style={{ margin: 0 }}>
+          <div className="clearfix">
+            <form onSubmit={this.preventReload}>
+              <a
+                className="emailInput"
+                href="mailto:nadiacoralves@gmail.com?Subject=Hi%20Nadia"
+                target="_top"
+                // className="submitEmail"
+                style={{ color: "black" }}
+              >
+                <input
+                  className=" float-left"
+                  type="text"
+                  placeholder="Email"
+                />
+                <FontAwesomeIcon icon={faChevronRight} size="1x" />
+              </a>
+            </form>
+            <span className="infoIcon">
+              <FontAwesomeIcon icon={faInfoCircle} size="1x" />
+            </span>
+            <span className="avocadoIcon" role="img" aria-label="Avocado">
+              <a
+                href="https://nadiacoralves.tumblr.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                🥑
+              </a>
+            </span>
+          </div>
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default Search;
