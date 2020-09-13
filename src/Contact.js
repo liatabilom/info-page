a829728d122be9dc9fe11734aaebeee46ebf83a0
nadiacoralves <55755193+nadiacoralves@users.noreply.github.com>
2020-09-13 13:38:48 +0100

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Card3 extends Component {
-	render() {
-		return (
-			<div>
-				<h1 className="subtitle">CONTACT</h1>
-				<h2 className="experienceText">
-					<div style={{ height: 80 }}>
-						<p>
-							correiagoncalvesnadia@gmail.com
-							<br />
-							<br />
-							+351932836572
-						</p>
-					</div>
-				</h2>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <h1 className="subtitle">CONTACT</h1>
+        <h2 className="experienceText">
+          <div style={{ height: 80 }}>
+            <p>
+              correiagoncalvesnadia@gmail.com
+              <br />
+              <br />
+            </p>
+          </div>
+        </h2>
+      </div>
+    );
+  }
 }
 
 export default Card3;
